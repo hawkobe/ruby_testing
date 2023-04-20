33de2745215474310bc8417237a79b251130e2c7
Jacob Sanders <sanders.jms@gmail.com>
2023-04-19 22:41:08 -1000

    finished shared tests for 11b_cat_spec

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:moira) { described_class.new('moira', 'tuxedo', 'black/white') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when it shares a method name with another Class' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(moira).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(moira).to be_hiding
   end
 end
